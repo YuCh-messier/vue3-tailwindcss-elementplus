6dc693411580e8010b633b4534ee7eb500c5c250
ZLAPortable <1181119044@qq.com>
2022-07-29 01:31:01 +0800

    yes,a good beginning

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // : resolve(__dirname, 'src/pages/')
+      }
+    }
+  }
+})
